bcb20d402f64d058889a04f658b41d37d2f69d4b
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-07-29 12:58:31 +0100

    Modify code to make it work with this minimal repository.

--- bin/update_copyright_licence_header.sh
+++ bin/update_copyright_licence_header.sh
@@ -8,11 +8,11 @@ set -e
 # 1. Remove licence (all lines starting with //LIC//) from all oomph-lib *.h
 #    and *.cc files.
 # 2. Update end-year of the copyright licence in "cc_and_h_licence_block.txt".
-# 3. Apply licence prefix to all oomph-lib *.h *.cc and  files.
+# 3. Apply licence prefix to all oomph-lib *.h and *.cc files.
 #-------------------------------------------------------------------------------
 
-# Find *.h and *.cc files in oomph-lib distribution (i.e. exclude external_src)
-oomph_lib_h_and_cc_files=$(find demo_drivers src user_src user_drivers self_test bin \( -name '*.h' -o -name '*.cc' \) -exec ls {} \;)
+# Find *.h and *.cc files
+oomph_lib_h_and_cc_files=$(find src \( -name '*.h' -o -name '*.cc' \) -exec ls {} \;)
 
 # --------[ REMOVE LICENSE ]----------------------------------------------------
 
@@ -25,12 +25,14 @@ done
 
 # --------[ UPDATE COPYRIGHT YEAR ]---------------------------------------------
 
-current_year=$(date +"%Y")
+current_year=$(date +"%H")
+# current_year=$(date +"%Y")
+echo "GOING TO USE THE CURRENT DATE VALUE: "$current_year
 sed --in-place "s/Copyright (C) 2006-.* Matthias Heil and Andrew Hazel/Copyright (C) 2006-$current_year Matthias Heil and Andrew Hazel/g" bin/cc_and_h_licence_block.txt
 
 # --------[ APPLY LICENSE ]-----------------------------------------------------
 
-# Add the licence header back to each source file
+# Add the licence header back to delicenced files
 for file in $oomph_lib_h_and_cc_files; do
     echo "Licencing:" $file
     cat bin/cc_and_h_licence_block.txt $file >$file.licenced
