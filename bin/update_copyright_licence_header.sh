265a20545c4a7a9a98150d15db9d0205818192b3
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-08-10 21:33:34 +0100

    Update workflow file.

--- bin/update_copyright_licence_header.sh
+++ bin/update_copyright_licence_header.sh
@@ -4,47 +4,40 @@
 set -e
 
 #-------------------------------------------------------------------------------
-# Shell script to perform the following steps (in the described order):
-# 1. Remove licence (all lines starting with //LIC//) from all oomph-lib *.h
-#    and *.cc files.
-# 2. Update end-year of the copyright licence in "cc_and_h_licence_block.txt".
-# 3. Apply licence prefix to all oomph-lib *.h and *.cc files.
+# Shell script to update the copyright notice of all oomph-lib-owned files.
 #-------------------------------------------------------------------------------
 
-# Find *.h and *.cc files
-oomph_lib_h_and_cc_files=$(find src \( -name '*.h' -o -name '*.cc' \) -exec ls {} \;)
+# --------[ IDENTIFY COPYRIGHTED FILES ]----------------------------------------
 
-# --------[ REMOVE LICENSE ]----------------------------------------------------
+# Get the current filename; we need to remember to ignore it from the
+# grep/ripgrep search rule
+current_filename=$(basename "$0")
 
-# Remove the licence header from each source file
-for file in $oomph_lib_h_and_cc_files; do
-    echo "Delicencing:" $file
-    gawk -f bin/remove_licence.awk $file >$file.delicenced
-    mv -f $file.delicenced $file
-done
+printf "Searching for ripgrep... "
+if command -v rg &>/dev/null; then
+    # Ripgrep is found so use that (it's much quicker than grep)
+    printf "found!\n"
+    echo "Searching for files"
+    rg -l -g '!'"$current_filename" -e "Copyright (C) 2006-" -e " Matthias Heil and Andrew Hazel"
+    echo "Searching for files"
+    oomph_lib_copyrighted_files=$(rg -l -g '!'"$current_filename" -e "Copyright (C) 2006-" -e " Matthias Heil and Andrew Hazel")
+else
+    printf "not found!\n"
+    oomph_lib_copyrighted_files=$(grep . -R -l --exclude="$current_filename" -e "Copyright (C) 2006-" -e " Matthias Heil and Andrew Hazel")
+fi
+
+echo "files found"
 
 # --------[ UPDATE COPYRIGHT YEAR ]---------------------------------------------
 
-current_year=$(date +"%H")
-# current_year=$(date +"%Y")
-echo "GOING TO USE THE CURRENT DATE VALUE: "$current_year
-sed --in-place "s/Copyright (C) 2006-.* Matthias Heil and Andrew Hazel/Copyright (C) 2006-$current_year Matthias Heil and Andrew Hazel/g" bin/cc_and_h_licence_block.txt
+# The end-year to update the notice to
+current_year=$(date +"%Y")
 
-# --------[ APPLY LICENSE ]-----------------------------------------------------
-
-# Add the licence header back to delicenced files
-for file in $oomph_lib_h_and_cc_files; do
-    echo "Licencing:" $file
-    cat bin/cc_and_h_licence_block.txt $file >$file.licenced
-    gawk -f bin/remove_licence.awk $file.licenced >$file.delicenced
-    if (test $(diff $file $file.delicenced | wc -w) != 0); then
-        echo "Original and licenced/delicenced file don't match!"
-        echo "Please run bin/remove_licence.sh first."
-        echo "Filename: " $file
-        exit 1
-    else
-        echo "OK"
-        mv -f $file.licenced $file
-        rm $file.delicenced
-    fi
+# Update the licence header for each file
+for file in $oomph_lib_copyrighted_files; do
+    echo "Updating copyright notice for file:" $file
+    sed "s/Copyright (C) 2006-.* Matthias Heil and Andrew Hazel/Copyright (C) 2006-$current_year Matthias Heil and Andrew Hazel/g" $file >$file.updated
+    mv $file.updated $file
 done
+
+# ------------------------------------------------------------------------------
