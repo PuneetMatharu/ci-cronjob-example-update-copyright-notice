22cabedd4d62eea0ec335336e7419fd5a85d2292
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-08-14 10:48:41 +0100

    Update shell script and workflow.

--- bin/update_copyright_licence_header.sh
+++ bin/update_copyright_licence_header.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 # exit when any command fails
 set -e
@@ -9,31 +9,20 @@ set -e
 
 # --------[ IDENTIFY COPYRIGHTED FILES ]----------------------------------------
 
-# Get the current filename; we need to remember to ignore it from the
-# grep/ripgrep search rule
+# Get the current filename; we need to ignore it from the search rule
 current_filename=$(basename "$0")
 
-printf "Searching for ripgrep... "
-if command -v rg &>/dev/null; then
-    # Ripgrep is found so use that (it's much quicker than grep)
-    printf "found!\n"
-    echo "Searching for files"
-    rg -l -g '!'"$current_filename" -e "Copyright (C) 2006-" -e " Matthias Heil and Andrew Hazel"
-    echo "Searching for files"
-    oomph_lib_copyrighted_files=$(rg -l -g '!'"$current_filename" -e "Copyright (C) 2006-" -e " Matthias Heil and Andrew Hazel")
-else
-    printf "not found!\n"
-    oomph_lib_copyrighted_files=$(grep . -R -l --exclude="$current_filename" -e "Copyright (C) 2006-" -e " Matthias Heil and Andrew Hazel")
-fi
-
-echo "files found"
+# Search for all files containing the copyright notice
+oomph_lib_copyrighted_files=$(grep . -R -l --exclude="$current_filename" -e 'Copyright (C) 2006-' -e ' Matthias Heil and Andrew Hazel')
 
 # --------[ UPDATE COPYRIGHT YEAR ]---------------------------------------------
 
 # The end-year to update the notice to
 current_year=$(date +"%Y")
 
-# Update the licence header for each file
+# Update the licence header for each file. Don't use the "-i" flag for an
+# in-place edit as this flag is not supported by the BSD version of "sed" that
+# ships with macOS
 for file in $oomph_lib_copyrighted_files; do
     echo "Updating copyright notice for file:" $file
     sed "s/Copyright (C) 2006-.* Matthias Heil and Andrew Hazel/Copyright (C) 2006-$current_year Matthias Heil and Andrew Hazel/g" $file >$file.updated
